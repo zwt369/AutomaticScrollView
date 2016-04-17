dc1eaebbd055ed00d7381a9a3544264c375677c8
tony <369388828@qq.com>
2016-04-17 22:20:48 +0800

    Initial Commit

---
+++ AutomaticScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutomaticScrollView
+//
+//  Created by Tony Zhang on 16/4/17.
+//  Copyright © 2016年 Tony Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutomaticScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutomaticScrollView
+//
+//  Created by Tony Zhang on 16/4/17.
+//  Copyright © 2016年 Tony Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
